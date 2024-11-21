a4fc9335b42136e16fffbb0039e468fe02bb55f2
= <jhowell45@googlemail.com>
2024-11-21 13:03:33 +0000

    It's faster to use += over trying to index the array again.

--- loops/php/code.php
+++ loops/php/code.php
@@ -5,7 +5,7 @@ $r = rand(0, 10000);                                # Get a random number 0 <= r
 $a = array_fill(0, 10000, 0);    # Array of 10k elements initialized to 0
 for ($i = 0; $i < 10000; $i++) {                    # 10k outer loop iterations
     for ($j = 0; $j < 100000; $j++) {               # 100k inner loop iterations, per outer loop iteration
-        $a[$i] = $a[$i] + $j%$u;                    # Simple sum
+        $a[$i] += $j%$u;                    # Simple sum
     }
     $a[$i] += $r;                                   # Add a random value to each element in array
 }
