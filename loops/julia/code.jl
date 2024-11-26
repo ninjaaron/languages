2cf3c959e78348bd51ef1c7fb8144a3d5c11c9e1
Aaron Christianson <ninjaaron@gmail.com>
2024-11-26 16:01:48 +0100

    added inline macro

--- loops/julia/code.jl
+++ loops/julia/code.jl
@@ -4,8 +4,8 @@ function main()
     u = parse(Int32, ARGS[1])
     r::Int32 = rand(1:10_000)   # Julia is 1-indexed
     a = zero(Array{Int32}(undef, 10_000))
-    for i in 1:10_000
-        for j in 0:99_999
+    @inline for i in 1:10_000
+        @inline for j in 0:99_999
             a[i] = a[i] + j%u
         end
         a[i] += r
